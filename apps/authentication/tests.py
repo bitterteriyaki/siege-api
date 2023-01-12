05d9b16c828d7d91e79d0b58392879025d1044fe
kyomi <personal@kyomi.codes>
2023-01-11 23:44:57 -0300

    test(apps/auth): test base authentication class

--- apps/authentication/tests.py
+++ apps/authentication/tests.py
@@ -10,6 +10,7 @@ from django.urls import reverse
 from rest_framework.status import (
     HTTP_200_OK,
     HTTP_400_BAD_REQUEST,
+    HTTP_401_UNAUTHORIZED,
     HTTP_403_FORBIDDEN,
 )
 from rest_framework.test import APITestCase
@@ -170,3 +171,28 @@ class AuthenticationTestCase(APITestCase):
         details = error["details"]
         self.assertIn("password", details)
         self.assertIn(expected, details["password"])
+
+
+class TokenAuthenticationTestCase(APITestCase):
+    """Test case for the token authentication class."""
+
+    def setUp(self):
+        self.example = {
+            "email": "user@email.com",
+            "username": "user",
+            "password": "password",
+        }
+        self.user = User.objects.create_user(**self.example)
+
+    def test_invalid_token_one_term(self):
+        url = reverse("users:get", kwargs={"target": "me"})
+        resp = self.client.get(url, HTTP_AUTHENTICATION="Token")
+
+        self.assertEqual(resp.status_code, HTTP_401_UNAUTHORIZED)
+
+    def test_invalid_token_three_terms(self):
+        url = reverse("users:get", kwargs={"target": "me"})
+        token = f"Token {self.user.token} another-term"
+
+        resp = self.client.get(url, HTTP_AUTHENTICATION=token)
+        self.assertEqual(resp.status_code, HTTP_401_UNAUTHORIZED)
