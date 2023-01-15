9ac03b907af95b7d2a13050e1ffa1b7c0ea3658c
kyomi <personal@kyomi.codes>
2023-01-15 12:43:05 -0300

    docs(apps/guilds): add documentation for `Guild` model

--- apps/guilds/models.py
+++ apps/guilds/models.py
@@ -13,8 +13,15 @@ from core.models import TimestampedModel
 
 
 class Guild(TimestampedModel):
-    """ """
+    # The name of the guild. The name can be updated by members with
+    # the permission to do so.
+    name = CharField(max_length=128)
 
-    name = CharField(max_length=126)
+    # The owner of the guild. The owner is the only one who can
+    # delete the guild. The owner can also transfer ownership to
+    # another user.
     owner_id = ForeignKey(User, on_delete=CASCADE)
+
+    # The description of the guild. This is optional. The description
+    # can be updated by members with the permission to do so.
     description = CharField(max_length=255, blank=True, null=True)
