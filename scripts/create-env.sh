01b645dce6d598f4b7dd0b4c3a1d6a900f20bf55
kyomi <personal@kyomi.codes>
2023-02-06 15:07:01 -0300

    chore: use undercased texts convention on sh scripts

--- scripts/create-env.sh
+++ scripts/create-env.sh
@@ -1,8 +1,8 @@
-# Copy the example environment file:
+# copy the example environment file:
 cp config/.env.example config/.env
 
-# Create a secret key for the application:
+# create a secret key for the application:
 SECRET_KEY=$(python3 -c 'from django.utils.crypto import get_random_string; print(get_random_string(64))')
 
-# Set the secret key in the environment file:
+# set the secret key in the environment file:
 sed -i "s/__EDIT_ME__/$SECRET_KEY/" config/.env
