338a13ea3b728570ebb713c0e92792c7eade5877
kyomi <personal@kyomi.codes>
2023-02-21 14:36:07 -0300

    feat(authentication): rewrite authentication app

--- server/urls.py
+++ server/urls.py
@@ -12,10 +12,7 @@ urlpatterns = (
     # apps:
     path("", include("apps.users.urls", namespace="users")),
     path("", include("apps.guilds.urls", namespace="guilds")),
-    re_path(
-        r"^api/",
-        include("apps.authentication.urls", namespace="authentication"),
-    ),
+    path("", include("apps.authentication.urls", namespace="authentication")),
     re_path(r"^api/", include("apps.members.urls", namespace="members")),
     re_path(r"^api/", include("apps.channels.urls", namespace="channels")),
 )
